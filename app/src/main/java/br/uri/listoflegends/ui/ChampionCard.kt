207407570e2bf89082bf727576bb340922b1d65a
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-09-28 16:05:36 -0300

    list fetching champions

---
+++ app/src/main/java/br/uri/listoflegends/ui/ChampionCard.kt
@@ -0,0 +1,75 @@
+package br.uri.listoflegends.ui
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import br.uri.listoflegends.models.ChampionModel
+import br.uri.listoflegends.services.getImageFromUrl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Composable
+fun ChampionCard(champion: ChampionModel) {
+    val coroutineScope = rememberCoroutineScope()
+    var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+
+    LaunchedEffect(champion.icon) {
+        coroutineScope.launch(Dispatchers.IO) {
+            bitmap = getImageFromUrl(champion.icon)
+        }
+    }
+
+    Card(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .padding(8.dp)
+            .size(150.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            bitmap?.let {
+                Image(
+                    bitmap = it.asImageBitmap(),
+                    contentDescription = champion.name,
+                    modifier = Modifier
+                        .size(125.dp)
+                        .padding(bottom = 12.dp) // Add padding here
+                )
+            }
+            Text(
+                text = champion.name,
+                modifier = Modifier.align(Alignment.BottomCenter)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/br/uri/listoflegends/ui/ChampionList.kt
@@ -0,0 +1,81 @@
+package br.uri.listoflegends.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import br.uri.listoflegends.R
+import br.uri.listoflegends.models.ChampionModel
+import br.uri.listoflegends.services.fetchChampions
+
+@Composable
+fun ChampionList() {
+    var champions by remember { mutableStateOf<List<ChampionModel>?>(null) }
+    var searchQuery by remember { mutableStateOf("") }
+    var responseCode by remember { mutableStateOf(-1) }
+
+    LaunchedEffect(Unit) {
+        fetchChampions { code, response ->
+            responseCode = code
+            champions = response
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            BasicTextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                textStyle = TextStyle(color = Color.Black),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(Color.LightGray, shape = MaterialTheme.shapes.medium)
+                    .padding(16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (champions == null) {
+                Text("Loading...")
+            } else {
+                val filteredChampions = if (searchQuery.isEmpty()) {
+                    champions
+                } else {
+                    champions!!.filter { it.name.contains(searchQuery, ignoreCase = true) }
+                }
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    contentPadding = PaddingValues(8.dp),
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    items(filteredChampions!!) { champion ->
+                        ChampionCard(champion)
+                    }
+                }
+            }
+        }
+    }
+}
