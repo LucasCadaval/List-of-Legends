5095b1730af046a20842186fd1c97e1f045f1281
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-09-29 16:00:44 -0300

    logo and splash screen

---
+++ app/src/main/java/br/uri/listoflegends/ui/SplashScreen.kt
@@ -0,0 +1,66 @@
+package br.uri.listoflegends.ui
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import br.com.uri.champions.ui.theme.BlueLol
+import br.com.uri.champions.ui.theme.GoldLol
+import br.uri.listoflegends.R
+
+@Composable
+fun SplashScreen(onTimeout: () -> Unit) {
+    val alpha = remember { Animatable(0f) }
+
+    LaunchedEffect(true) {
+        alpha.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
+        )
+        kotlinx.coroutines.delay(2000)
+        onTimeout()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BlueLol),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                modifier = Modifier.graphicsLayer(alpha = alpha.value)
+            )
+
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .padding(16.dp),
+                color = GoldLol
+            )
+        }
+    }
+}
