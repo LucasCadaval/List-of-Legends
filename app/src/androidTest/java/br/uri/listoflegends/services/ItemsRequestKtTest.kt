c751e23579bf40e81d6aa2d27c232ae6c14ca012
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-11-12 16:22:11 -0300

    item request test

---
+++ app/src/androidTest/java/br/uri/listoflegends/services/ItemsRequestKtTest.kt
@@ -0,0 +1,60 @@
+import android.content.Context
+import android.util.Log
+import androidx.test.core.app.ApplicationProvider
+import br.uri.listoflegends.models.ItemModel
+import br.uri.listoflegends.services.fetchItems
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+class FetchItemsTest {
+    private lateinit var mockWebServer: MockWebServer
+
+    @Mock
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+        MockitoAnnotations.openMocks(this)
+        mockWebServer = MockWebServer()
+        mockWebServer.start(3001)
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun fetchItemsReturnsItemsSuccessfully() = runTest {
+        val mockJsonResponse = MockResponse().setBody("")
+        mockWebServer.enqueue(mockJsonResponse)
+
+        val items = mutableListOf<ItemModel>()
+        var responseCode: Int? = null
+        val latch = CountDownLatch(1)
+
+        fetchItems(context) { code, result ->
+            responseCode = code
+            items.addAll(result!!)
+            latch.countDown()
+        }
+
+        latch.await(5, TimeUnit.SECONDS)
+
+        assertEquals(20, items.size)
+        assertEquals(200, responseCode)
+        assertEquals("Boots of Speed", items[0].name)
+    }
+}
