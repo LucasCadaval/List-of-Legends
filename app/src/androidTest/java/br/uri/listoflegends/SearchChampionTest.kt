4d350b5c331b2ebf302eb380e8f6f4bb4b8da76d
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-11-17 12:20:29 -0300

    rename

--- app/src/androidTest/java/br/uri/listoflegends/SearchChampionTest.kt
+++ app/src/androidTest/java/br/uri/listoflegends/SearchChampionTest.kt
@@ -1,8 +1,6 @@
 package br.uri.listoflegends
 
 import android.content.Context
-import android.provider.Settings.Global.getString
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
@@ -10,23 +8,21 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import br.uri.listoflegends.ui.ChampionList
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ChampionListUITest {
+class SearchChampionTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
     private val context: Context = ApplicationProvider.getApplicationContext()
 
-
     @Test
-    fun searchAndClickOnAatrox() {
+    fun searchAndClickOnAphelios() {
 
         val searchHint: String = context.getString(R.string.search_hint)
 
